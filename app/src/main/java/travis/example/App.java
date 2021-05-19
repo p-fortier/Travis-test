6afd3adbebdc362f4ca2b5b93e391d6064c3ad8b
Patrik Fortier <patrik.fortier@insa-lyon.fr>
2021-05-19 10:57:03 +0200

    i fixed an error

--- app/src/main/java/travis/example/App.java
+++ app/src/main/java/travis/example/App.java
@@ -1,10 +1,8 @@
 package travis.example;
 
-import sun.jvm.hotspot.HelloWorld;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println(new App().HelloWorld())
+        System.out.println(new App().HelloWorld());
     }
 
     private String HelloWorld(){
