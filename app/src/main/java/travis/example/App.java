01290f1a69a3437098ca922f7afbca4b719ae639
Patrik Fortier <patrik.fortier@insa-lyon.fr>
2021-05-19 10:55:02 +0200

    i made a modification

--- app/src/main/java/travis/example/App.java
+++ app/src/main/java/travis/example/App.java
@@ -1,7 +1,13 @@
 package travis.example;
 
+import sun.jvm.hotspot.HelloWorld;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(new App().HelloWorld());
+    }
+
+    private String HelloWorld(){
+        return "HelloWorld";
     }
 }
